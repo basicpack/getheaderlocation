a52323cf41df55e5aa80c852d401f210efad23d4
Anderson Ismael <23392311+aicouto@users.noreply.github.com>
2019-07-31 09:56:28 -0300

    M  getheaderlocation.php

--- getheaderlocation.php
+++ getheaderlocation.php
@@ -3,13 +3,18 @@
 //05 de outrubro de 2018
 
 function getHeaderLocation($url){
-    if (filter_var($url, FILTER_VALIDATE_URL) !== false) {
-        $header=get_headers($url,true);
-    }
-    if(isset($header['Location'])){
-        $url=$header['Location'];
-        return getHeaderLocation($url);
-    }else{
-        return $url;
-    }
+    $userAgent='Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0';
+    $tempoMaximoEmSegundos=10;
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_HEADER  , true);  // we want headers
+    curl_setopt($ch, CURLOPT_NOBODY  , true);  // we don't need body
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, $tempoMaximoEmSegundos);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);
+    $result = curl_exec($ch);
+    $redirectUrl = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL );
+    curl_close($ch);
+    return $redirectUrl;
 }
